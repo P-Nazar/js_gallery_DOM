151890c28ce595baeccb3ba71ed78c36ef9f7926
Nazar Pastuschak <kns19pna@gmail.com>
2024-10-04 22:27:37 +0300

    make build

---
+++ index.9a7af3d1.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var n=t.target;e.src=n.closest(".list-item__link").href});
+//# sourceMappingURL=index.9a7af3d1.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const largeImg = document.getElementById("largeImg");
+const thumbs = document.getElementById("thumbs");
+// eslint-disable-next-line no-shadow
+thumbs.addEventListener("click", (event)=>{
+    event.preventDefault();
+    const target = event.target;
+    largeImg.src = target.closest(".list-item__link").href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
